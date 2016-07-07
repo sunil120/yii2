f445988d0f89ddde2ea2d017bd5d43f2777b5407
Sunil Kumar <skumar@cygnet-infotech.com>
2016-07-07 18:14:49 +0530

    added vender

--- backend/config/params.php
+++ backend/config/params.php
@@ -2,6 +2,6 @@
 return [
     'adminEmail' => 'admin@example.com',
     'webUrl'     => 'http://' . $_SERVER['HTTP_HOST'],
-    'adminUrl'    => 'http://' . $_SERVER['HTTP_HOST'].'/backend/',
-    'fileUrl'   => 'http://' . $_SERVER['HTTP_HOST'].'/backend/web/'
+    'adminUrl'    => 'http://' . $_SERVER['HTTP_HOST'].'/advanced/backend/',
+    'fileUrl'   => 'http://' . $_SERVER['HTTP_HOST'].'/advanced/backend/web/'
 ];
